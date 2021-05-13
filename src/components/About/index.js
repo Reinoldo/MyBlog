69d4f5df5b156fecc19003cf773068e3f419dce3
Reinoldo <reinoldo@outlook.com>
2021-05-13 11:58:38 +0100

    About component added. Open authoring set to true

---
+++ src/components/About/index.js
@@ -0,0 +1,16 @@
+import * as S from "./styled"
+import React from "react"
+const About = () => (
+  <S.AboutWrapper>
+    <h1>About me</h1>
+    <p>My name is Reinoldo Sommer and I'm from Brazil :)</p>
+    <p>
+      I'm a software developer, graduated in 2017 who worked as a full-stack dev
+      for about a year and decided to focus on front end technologies such as
+      React, GraphQl, NextJs and JavaScript.
+    </p>
+    <p></p>
+  </S.AboutWrapper>
+)
+
+export default About
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react"
 
-import Layout from '../components/Layout'
-import SEO from '../components/seo'
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+import About from "../components/About"
 
 const AboutPage = () => {
   return (
     <>
       <Layout>
-        <SEO title='About' />
-        <h1>About us</h1>
+        <SEO title="About" />
+
+        <About />
       </Layout>
     </>
   )
